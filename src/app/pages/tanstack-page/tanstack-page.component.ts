217673098ccc3695d33c7bf3c2ab531574a2e7b2
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2024-07-12 17:15:07 +0200

    fix: tanstack page

---
+++ src/app/pages/tanstack-page/tanstack-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-tanstack",
+  standalone: true,
+  imports: [],
+  templateUrl: "./tanstack-page.component.html",
+})
+export default class TanstackComponent {}
