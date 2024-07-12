7a953f038a040ce54751478d487e934bdccf42bf
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2024-07-12 17:41:16 +0200

    feat: first version of tanstack

--- src/app/pages/tanstack-page/tanstack-page.component.ts
+++ src/app/pages/tanstack-page/tanstack-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
+import { ColumnSearchComponent } from "@components/tanstack-table/filtering/column-search/column-search.component";
 
 @Component({
   selector: "app-tanstack",
   standalone: true,
-  imports: [],
+  imports: [ColumnSearchComponent],
   templateUrl: "./tanstack-page.component.html",
 })
 export default class TanstackComponent {}
