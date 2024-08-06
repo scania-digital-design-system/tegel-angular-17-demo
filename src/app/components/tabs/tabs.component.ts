84a44191e1b4c1363615db4717b5b235f015b7d4
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2024-08-06 10:38:34 +0200

    chore: inject inline tabs

--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { TegelModule } from "@scania/tegel-angular-17";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-tabs",
@@ -23,20 +24,12 @@ import { TegelModule } from "@scania/tegel-angular-17";
     </tds-folder-tabs>
 
     <div class="tds-headline-03 tds-u-pb3 tds-u-pt3">Inline</div>
-    <tds-inline-tabs>
-      <tds-inline-tab>
-        <button>First tab</button>
-      </tds-inline-tab>
-      <tds-inline-tab>
-        <button>Second tab is much longer</button>
-      </tds-inline-tab>
-      <tds-inline-tab>
-        <button>Third tab</button>
-      </tds-inline-tab>
-      <tds-inline-tab disabled>
-        <button>Fourth tab</button>
+    <tds-inline-tabs defaultSelectedIndex="1" *ngIf="loaded">
+      <tds-inline-tab *ngFor="let tab of inlineTabs">
+        <button>{{ tab }}</button>
       </tds-inline-tab>
     </tds-inline-tabs>
+    <button (click)="injectInlineTab()">Inject inline tabs</button>
 
     <div class="tds-headline-03 tds-u-pb3 tds-u-pt3">Navigation</div>
     <tds-navigation-tabs>
@@ -54,6 +47,18 @@ import { TegelModule } from "@scania/tegel-angular-17";
       </tds-navigation-tab>
     </tds-navigation-tabs>
   `,
-  imports: [TegelModule],
+  imports: [TegelModule, CommonModule],
 })
-export class TabsComponent {}
+export class TabsComponent {
+  loaded = false;
+  inlineTabs: string[] = [
+    "First tab",
+    "Second tab is much longer",
+    "Third tab",
+    "Fourth tab",
+  ];
+
+  injectInlineTab() {
+    this.loaded = true;
+  }
+}
