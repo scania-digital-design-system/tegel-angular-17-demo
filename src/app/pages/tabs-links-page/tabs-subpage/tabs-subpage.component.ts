a86853a5a747368392b8d02e3080f457d51e584c
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2024-10-25 14:48:51 +0200

    chore(tooltip): support example

--- src/app/pages/tabs-links-page/tabs-subpage/tabs-subpage.component.ts
+++ src/app/pages/tabs-links-page/tabs-subpage/tabs-subpage.component.ts
@@ -1,12 +1,45 @@
 import { Component, Input } from "@angular/core";
 import { TegelModule } from "@scania/tegel-angular-17";
+import { CommonModule } from "@angular/common";
+import data from "./data.json";
 
 @Component({
   selector: "app-tabs-subpage",
   standalone: true,
   templateUrl: "./tabs-subpage.component.html",
-  imports: [TegelModule],
+  imports: [TegelModule, CommonModule],
+  styles: [
+    `
+      #customers {
+        font-family: Arial, Helvetica, sans-serif;
+        border-collapse: collapse;
+      }
+
+      #customers td,
+      #customers th {
+        border: 1px solid #ddd;
+        padding: 8px;
+      }
+
+      #customers tr:nth-child(even) {
+        background-color: #f2f2f2;
+      }
+
+      #customers tr:hover {
+        background-color: #ddd;
+      }
+
+      #customers th {
+        padding-top: 12px;
+        padding-bottom: 12px;
+        text-align: left;
+        background-color: #04aa6d;
+        color: white;
+      }
+    `,
+  ],
 })
 export default class TabsSubpageComponent {
   @Input() pathIndex: number;
+  tableData = data;
 }
