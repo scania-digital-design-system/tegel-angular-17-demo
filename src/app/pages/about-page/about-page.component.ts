53982db8f7829a2731fd06e0fe0c77df674c1e78
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2024-11-25 15:46:25 +0100

    chore: example for issue 716

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -1,10 +1,10 @@
 import { Component } from "@angular/core";
 import { TegelModule } from "@scania/tegel-angular-17";
-
+import { ExampleProductListComponent } from "../test/test.components";
 @Component({
   selector: "app-about-page",
   standalone: true,
   templateUrl: "./about-page.component.html",
-  imports: [TegelModule],
+  imports: [TegelModule, ExampleProductListComponent],
 })
 export default class AboutPageComponent {}
