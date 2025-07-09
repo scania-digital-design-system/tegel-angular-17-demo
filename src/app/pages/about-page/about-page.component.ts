90692c3fb29b362350a29f32cb596c41da46cf18
Charles Krook <charles.krook@gmail.com>
2025-07-09 11:31:46 +0200

    feat(about-page): update dropdown handling and import JsonPipe

--- src/app/pages/about-page/about-page.component.ts
+++ src/app/pages/about-page/about-page.component.ts
@@ -1,10 +1,22 @@
 import { Component } from "@angular/core";
+import { JsonPipe } from "@angular/common";
 import { TegelModule } from "@scania/tegel-angular-17";
 
 @Component({
   selector: "app-about-page",
   standalone: true,
   templateUrl: "./about-page.component.html",
-  imports: [TegelModule],
+  imports: [TegelModule, JsonPipe],
 })
-export default class AboutPageComponent {}
+export default class AboutPageComponent {
+  selectedValues: { [key: string]: any } = {};
+  Array = Array; // Make Array available in template
+
+  handleDropdownChange(event: Event, dropdownName: string) {
+    const target = event.target as HTMLTdsDropdownElement;
+    this.selectedValues = {
+      ...this.selectedValues,
+      [dropdownName]: target.value,
+    };
+  }
+}
