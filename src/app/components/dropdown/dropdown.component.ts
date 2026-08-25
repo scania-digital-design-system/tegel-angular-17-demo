91b0223fb4f25166f33041856f9321762f3638e4
Filipa Neto <filipa.neto@scania.com>
2026-08-25 08:54:53 +0100

    feat(dropdown): add other dropdown scenarios to showcase functionalities
    Co-authored-by: Clara Severino <clara.severino@scania.com>
    Co-authored-by: João Gonçalves <joao.x.goncalves@scania.com>

--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -8,15 +8,15 @@ import { TegelModule } from "@scania/tegel-angular-17";
   imports: [TegelModule],
 })
 export default class DropdownComponent {
-  @ViewChild("dropdownRef", { static: true })
-  dropdownRef: HTMLTdsDropdownElement;
+  // @ViewChild("dropdownRef", { static: true })
+  // dropdownRef: HTMLTdsDropdownElement;
 
-  resetDropdown() {
-    // Check if the dropdown component instance is accessible
-    if (this.dropdownRef) {
-      this.dropdownRef.reset();
-    } else {
-      console.log("Dropdown reference is NOT valid, cannot call reset.");
-    }
-  }
+  // resetDropdown() {
+  //   // Check if the dropdown component instance is accessible
+  //   if (this.dropdownRef) {
+  //     this.dropdownRef.reset();
+  //   } else {
+  //     console.log("Dropdown reference is NOT valid, cannot call reset.");
+  //   }
+  // }
 }
